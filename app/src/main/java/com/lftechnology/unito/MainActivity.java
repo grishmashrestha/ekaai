0add0d15bf291c06c8aa2c5c20c1ad5d80d5d923
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-24 16:06:18 +0545

    remove unnecessary Activities

--- app/src/main/java/com/lftechnology/unito/MainActivity.java
+++ app/src/main/java/com/lftechnology/unito/MainActivity.java
@@ -3,20 +3,34 @@ package com.lftechnology.unito;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @Bind(R.id.inflated_view_parent) le
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        LengthFragment lengthFragment = new LengthFragment();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.inflated_view_parent, lengthFragment, "LengthFragment");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     @Override
