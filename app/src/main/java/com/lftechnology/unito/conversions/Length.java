8c5aafc9673a6ea66bba207b9ca35231bf46d10f
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-02 11:23:28 +0545

    add conversion theme for unit conversion

---
+++ app/src/main/java/com/lftechnology/unito/conversions/Length.java
@@ -0,0 +1,18 @@
+package com.lftechnology.unito.conversions;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/1/16.
+ */
+public class Length {
+//    private Float nauticalMile, kilometer, centimeter, yard, inch, feet, meter, millimeter, mile, micrometer, nanometer, micron;
+
+    private void getConstant(String from, String to) {
+
+    }
+
+    public double convert(Float value, String from, String to) {
+        return 1.0;
+    }
+
+
+}
---
+++ app/src/main/java/com/lftechnology/unito/conversions/Weight.java
@@ -0,0 +1,7 @@
+package com.lftechnology.unito.conversions;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/1/16.
+ */
+public class Weight {
+}
