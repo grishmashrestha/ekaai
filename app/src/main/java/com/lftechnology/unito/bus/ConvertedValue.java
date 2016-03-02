299877b6d3649bdde8e6e6e46a7a16dc2093fe0a
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-02 14:36:17 +0545

    create a class ConvertedValue to pass through one fragment to another fragment

---
+++ app/src/main/java/com/lftechnology/unito/bus/ConvertedValue.java
@@ -0,0 +1,12 @@
+package com.lftechnology.unito.bus;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/2/16.
+ */
+public class ConvertedValue {
+    public String  value;
+
+    public ConvertedValue(String val) {
+        value = val;
+    }
+}
