e629ce831c4149dabd3b1c65214815c341d0c69a
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-03 09:33:27 +0545

    add attribute to FragmentDetail class

--- app/src/main/java/com/lftechnology/unito/FragmentDetails.java
+++ app/src/main/java/com/lftechnology/unito/FragmentDetails.java
@@ -4,4 +4,6 @@ package com.lftechnology.unito;
  * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/2/16.
  */
 public class FragmentDetails {
+    public String[] dataset;
+    public String fragmentClass;
 }
