4a8d584f3c994704f31e04ea89f3904efcdec2a2
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-03 09:31:53 +0545

    add app constants

---
+++ app/src/main/java/com/lftechnology/unito/constant/AppConstant.java
@@ -0,0 +1,19 @@
+package com.lftechnology.unito.constant;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/3/16.
+ */
+public class AppConstant {
+    public static final String LENGTH = "Length";
+    public static final String TEMPERATURE = "Temperature";
+    public static final String WEIGHT = "Weight";
+    public static final String VOLUME = "Volume";
+    public static final String TIME = "Time";
+
+    public static final String LENGTH_FRAGMENT_NAME = "com.lftechnology.unito.fragment.LengthFragment";
+    public static final String TEMPERATURE_FRAGMENT_NAME = "com.lftechnology.unito.fragment.TemperatureFragment";
+    public static final String WEIGHT_FRAGMENT_NAME = "com.lftechnology.unito.fragment.WeightFragment";
+    public static final String VOLUME_FRAGMENT_NAME = "com.lftechnology.unito.fragment.VolumeFragment";
+    public static final String TIME_FRAGMENT_NAME = "com.lftechnology.unito.fragment.TimeFragment";
+
+}
