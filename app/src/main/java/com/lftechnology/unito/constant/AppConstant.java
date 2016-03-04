b70d47736eb488d15db05ce0c1da1fc4559691c4
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-04 16:27:24 +0545

    add new constants to AppConstant

--- app/src/main/java/com/lftechnology/unito/constant/AppConstant.java
+++ app/src/main/java/com/lftechnology/unito/constant/AppConstant.java
@@ -15,4 +15,25 @@ public class AppConstant {
     public static final String WEIGHT_FRAGMENT_NAME = "com.lftechnology.unito.fragment.WeightFragment";
     public static final String VOLUME_FRAGMENT_NAME = "com.lftechnology.unito.fragment.VolumeFragment";
     public static final String TIME_FRAGMENT_NAME = "com.lftechnology.unito.fragment.TimeFragment";
+
+    public class LengthConstant {
+        public static final String NAUTICAL_MILE = "Nautical Mile";
+        public static final String KILOMETER = "Kilometre (km)";
+        public static final String CENTIMETER = "Centimeter (cm)";
+        public static final String YARD = "Yard (yd)";
+        public static final String INCH = "Inch (in)";
+        public static final String FEET = "Feet (ft)";
+        public static final String METER = "Meter (m)";
+        public static final String MILLIMETER = "Millimeter (mm)";
+        public static final String MILE = "Mile (ml)";
+        public static final String MICROMETER = "Micrometers (um)";
+        public static final String NANOMETER = "Nanometers (nm)";
+        public static final String MICRON = "Micron (u)";
+    }
+
+    public class TemperatureConstant {
+        public static final String CELSIUS = "Celsius (C)";
+        public static final String FAHRENHEIT = "Fahrenheit (F)";
+        public static final String KELVIN = "kelvin (K)";
+    }
 }
