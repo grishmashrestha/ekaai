181a7a309ab6f080d250fad86601df0983aed080
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-04 11:11:27 +0545

    create separate class under utils to handle AutoResizing of TextView fonts

---
+++ app/src/main/java/com/lftechnology/unito/utils/AutoResizeFontTextView.java
@@ -0,0 +1,30 @@
+package com.lftechnology.unito.utils;
+
+import android.widget.TextView;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/4/16.
+ */
+public class AutoResizeFontTextView {
+
+    public static void changeFontSize(CharSequence s, TextView et) {
+        float stringLength = s.length();
+        float fontSize;
+        if (stringLength <= 6) {
+            fontSize = 100;
+        } else if (stringLength <= 7) {
+            fontSize = 90;
+        } else if (stringLength <= 8) {
+            fontSize = 80;
+        } else if (stringLength <= 9) {
+            fontSize = 70;
+        } else if (stringLength <= 10) {
+            fontSize = 60;
+        } else if (stringLength <= 12) {
+            fontSize = 50;
+        } else {
+            fontSize = 20;
+        }
+        et.setTextSize(fontSize);
+    }
+}
