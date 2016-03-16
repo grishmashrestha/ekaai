c082f7f6cc8c5e9e8832dc394b0793d2c68e775c
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-16 11:22:59 +0545

    try Mockito

---
+++ app/src/main/java/com/lftechnology/unito/testing/PersonChecker.java
@@ -0,0 +1,12 @@
+package com.lftechnology.unito.testing;
+
+import timber.log.Timber;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/16/16.
+ */
+public class PersonChecker {
+    public void checkAge(Person person) {
+        System.out.println(person.getAge());
+    }
+}
