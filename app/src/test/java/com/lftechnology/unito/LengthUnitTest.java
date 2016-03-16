0c0295f388d785d79d16acbf6bdd4070422499ea
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-16 10:10:48 +0545

    create new test for Length Conversion

---
+++ app/src/test/java/com/lftechnology/unito/LengthUnitTest.java
@@ -0,0 +1,16 @@
+package com.lftechnology.unito;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/16/16.
+ */
+public class LengthUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
