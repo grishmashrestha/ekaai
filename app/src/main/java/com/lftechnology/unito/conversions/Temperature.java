ec1fff4046175b2a6738329aab1b5af6e737aec1
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-03-31 14:43:19 +0545

    make Temperature class extend Unit abstract class and override its methods accordingly

--- app/src/main/java/com/lftechnology/unito/conversions/Temperature.java
+++ app/src/main/java/com/lftechnology/unito/conversions/Temperature.java
@@ -5,18 +5,19 @@ import com.lftechnology.unito.constant.AppConstant;
 /**
  * Created by Grishma Shrestha <grishmashrestha@lftechnology.com> on 3/1/16.
  */
-public class Temperature {
-    private String to, from;
-    private Double value;
+public class Temperature extends Unit {
 
-    public Temperature (Double val, String from, String to) {
-        value = val;
-        this.to = to;
-        this.from = from;
+    public Temperature(Double val, String from, String to) {
+        super(val, from, to);
     }
 
-    public double convert() {
-        double returnValue;
+    @Override
+    public Double convert() {
+        Double returnValue;
+        Double value = getValue();
+        String from = getFrom();
+        String to = getTo();
+
         switch (from) {
             case AppConstant.TemperatureConstant.CELSIUS:
                 switch (to) {
@@ -24,28 +25,28 @@ public class Temperature {
                         returnValue = value;
                         break;
                     case AppConstant.TemperatureConstant.FAHRENHEIT:
-                        returnValue = (((value * 9)/5) + 32);
+                        returnValue = (((value * 9) / 5) + 32);
                         break;
                     case AppConstant.TemperatureConstant.KELVIN:
                         returnValue = value + 273.15;
                         break;
                     default:
-                        returnValue = 0;
+                        returnValue = 0.0;
                 }
                 break;
             case AppConstant.TemperatureConstant.FAHRENHEIT:
                 switch (to) {
                     case AppConstant.TemperatureConstant.CELSIUS:
-                        returnValue = ((value - 32) * 5/9);
+                        returnValue = ((value - 32) * 5 / 9);
                         break;
                     case AppConstant.TemperatureConstant.FAHRENHEIT:
                         returnValue = value;
                         break;
                     case AppConstant.TemperatureConstant.KELVIN:
-                        returnValue = ((value + 459.67) * 5/9);
+                        returnValue = ((value + 459.67) * 5 / 9);
                         break;
                     default:
-                        returnValue = 0;
+                        returnValue = 0.0;
                 }
                 break;
             case AppConstant.TemperatureConstant.KELVIN:
@@ -54,13 +55,13 @@ public class Temperature {
                         returnValue = (value - 273.15);
                         break;
                     case AppConstant.TemperatureConstant.FAHRENHEIT:
-                        returnValue = ((value * (9/5)) - 459.67);
+                        returnValue = ((value * (9 / 5)) - 459.67);
                         break;
                     case AppConstant.TemperatureConstant.KELVIN:
                         returnValue = value;
                         break;
                     default:
-                        returnValue = 0;
+                        returnValue = 0.0;
                 }
                 break;
             default:
